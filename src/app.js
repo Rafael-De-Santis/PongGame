3b5614442e817ae5dadc2166eb1515dbb538292b
d4tapunk <d4tapunk@gmail.com>
2023-02-21 15:36:30 -0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+//Players
+const playerRight = document.getElementById("playerRight");
+const playerLeft = document.getElementById("playerLeft");
+
+
+window.addEventListener("keydown", (element) =>{
+    console.log(element.key)
+
+    switch (element.key) {
+        case "ArrowUp"
+    }
+    
+})
