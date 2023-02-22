7a5d6dd2ec81b6deeb8b5c3227e34740d12f1ff9
d4tapunk <d4tapunk@gmail.com>
2023-02-21 21:37:31 -0800

    second commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 //Players
 const playerRight = document.getElementById("playerRight");
 const playerLeft = document.getElementById("playerLeft");
+const field = document.getElementById("field");
+let maxWidth = field.offsetWidth;
+let maxHeight = field.offsetHeight;
+
+let playerLeftHeight = playerLeft.offsetHeight;
+let playerRightHeight = playerRight.offsetHeight;
+
+let playerInit = (maxHeight / 2) - (playerLeftHeight / 2);
 
 
-window.addEventListener("keydown", (element) =>{
+
+/* window.addEventListener("keydown", (element) =>{
     console.log(element.key)
 
     switch (element.key) {
@@ -11,3 +20,14 @@ window.addEventListener("keydown", (element) =>{
     }
     
 })
+
+ */
+/* window.innerWidth*/
+
+console.log(field.offsetWidth);
+
+/*! TODO POR AHORA
+1. Inicializar la position del player
+2. Crear controles del player (arriba y abajo)
+3. Crear los limetes del player
+*/
