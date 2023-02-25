46d9e0f77af9282b7ae65a8cf4c749a354bb7907
d4tapunk <d4tapunk@gmail.com>
2023-02-25 11:11:40 -0800

    playerRight movement

--- src/app.js
+++ src/app.js
@@ -10,19 +10,51 @@ let playerRightHeight = playerRight.offsetHeight;
 
 let playerInit = (maxHeight / 2) - (playerLeftHeight / 2);
 
+playerRight.style.top = playerInit + "px";
+playerLeft.style.top = playerInit + "px";
 
+//Player Movement----------------------------------------------------
 
-/* window.addEventListener("keydown", (element) =>{
+let keysPressed = {};
+
+window.addEventListener("keydown", (element) => {
     console.log(element.key)
 
-    switch (element.key) {
-        case "ArrowUp"
-    }
-    
-})
+    var currentPositionPR = playerRight.offsetTop;
 
+ /*    var currentPositionPL = playerLeft.offsetTop; */
+
+    if (currentPositionPR > 5) {
+        switch (element.key) {
+            case "ArrowUp": 
+                //console.log(currentPositionPR + "pa arriba");
+                playerRight.style.top = currentPositionPR - 5 + "px";
+                break;
+        }}
+
+    if(currentPositionPR < maxHeight-playerRightHeight - 16) {       7
+        switch(element.key) {
+            case "ArrowDown": 
+                //console.log(currentPositionPR + "pa abajo");
+                playerRight.style.top = currentPositionPR + 5 + "px";
+        }}
+
+/*     if (currentPositionPL > 5) {
+        switch (element.key) {
+            case "s": 
+                console.log(currentPositionPL + "pa arriba");
+                playerLeft.style.top = currentPositionPL - 5 + "px";
+                break;
+            }}
+    
+    if(currentPositionPL < maxHeight-playerLeftHeight - 16) {       
+        switch(element.key) {
+            case "x": 
+                console.log(currentPositionPL + "pa abajo");
+                playerLeft.style.top = currentPositionPL + 5 + "px";
+                }}
  */
-/* window.innerWidth*/
+})
 
 console.log(field.offsetWidth);
 
@@ -31,3 +63,6 @@ console.log(field.offsetWidth);
 2. Crear controles del player (arriba y abajo)
 3. Crear los limetes del player
 */
+
+
+
